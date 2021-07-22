2255320fc42ceaa1145096dac1d24836ca28b7b4
spectocr <highstakes22@gmail.com>
2021-07-21 20:06:33 -0400

    adding in login logic fixes, style updates, and adding in bootstrap link

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,68 +1,72 @@
 
 
 const loginForm = document.querySelector('.formcontainer');
+//console.log(loginForm);
 
 
-const handleAnimalFormSubmit = event => {
+const handleLoginFormSubmit = event => {
   event.preventDefault();
+  //console.log("submit captured")
+  
 
   // get user data 
   const userName = loginForm.querySelector('[name="uname"]').value;
   const password = loginForm.querySelector('[name="psw"]').value;
 
   const userObject = { userName, password };
+  console.log(userObject);
 
 
 
-  fetch('/api/users/login', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userObject)
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      alert(`Error: ${response.statusText}`);
-    })
-    .then(postResponse => {
-      console.log(postResponse);
-      alert('Thank you for adding an animal!');
-    });
+  // fetch('/api/users/login', {
+  //   method: 'POST',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify(userObject)
+  // })
+  //   .then(response => {
+  //     if (response.ok) {
+  //       return response.json();
+  //     }
+  //     alert(`Error: ${response.statusText}`);
+  //   })
+  //   .then(postResponse => {
+  //     console.log(postResponse);
+  //     alert('Thank you for adding an animal!');
+  //   });
 };
 
-const handleZookeeperFormSubmit = event => {
-  event.preventDefault();
+// const handleZookeeperFormSubmit = event => {
+//   event.preventDefault();
 
-  // get zookeeper data and organize it
-  const name = $zookeeperForm.querySelector('[name="zookeeper-name"]').value;
-  const age = parseInt($zookeeperForm.querySelector('[name="age"]').value);
-  const favoriteAnimal = $zookeeperForm.querySelector('[name="favorite-animal"]').value;
+//   // get zookeeper data and organize it
+//   const name = $zookeeperForm.querySelector('[name="zookeeper-name"]').value;
+//   const age = parseInt($zookeeperForm.querySelector('[name="age"]').value);
+//   const favoriteAnimal = $zookeeperForm.querySelector('[name="favorite-animal"]').value;
 
-  const zookeeperObj = { name, age, favoriteAnimal };
-  console.log(zookeeperObj);
-  fetch('api/zookeepers', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(zookeeperObj)
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      alert('Error: ' + response.statusText);
-    })
-    .then(postResponse => {
-      console.log(postResponse);
-      alert('Thank you for adding a zookeeper!');
-    });
-};
+//   const zookeeperObj = { name, age, favoriteAnimal };
+//   console.log(zookeeperObj);
+//   fetch('api/zookeepers', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(zookeeperObj)
+//   })
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       alert('Error: ' + response.statusText);
+//     })
+//     .then(postResponse => {
+//       console.log(postResponse);
+//       alert('Thank you for adding a zookeeper!');
+//     });
+// };
 
-$zookeeperForm.addEventListener('submit', handleZookeeperFormSubmit);
-$animalForm.addEventListener('submit', handleAnimalFormSubmit);
+loginForm.addEventListener('click', handleLoginFormSubmit);
+//$animalForm.addEventListener('submit', handleAnimalFormSubmit);
