c0e208a97421d23d5afa8a24017c3888d4c26108
mlopez <lopezmatthew87@gmail.com>
2021-07-23 11:19:23 -0400

    homepage loads using handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-// console.log(apiRoutes);
+
+const homeRoutes = require('./home-routes.js');
+
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
