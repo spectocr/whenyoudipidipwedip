7fab36b72ce1532e171b9b23ba2cef97a0f7244b
mlopez <lopezmatthew87@gmail.com>
2021-07-23 10:32:59 -0400

    adding handlebars and testing out routes for hbs

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const routes = require('./routes');
-// const htmlRoutes = require('./routes/htmlRoutes');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars'); // setting up handlebars
 
 // turn on routes
 app.use(routes);
-// app.use('/', htmlRoutes);
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
